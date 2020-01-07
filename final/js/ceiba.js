40a34cf7c18fa1f951a244a0c087a63e101a9ec4
YC <39358847+yuchyuann@users.noreply.github.com>
2020-01-07 18:03:02 +0800

    Add files via upload
    finall

---
+++ final/js/ceiba.js
@@ -0,0 +1,20 @@
+class ceiba {
+    constructor() {
+        //起始位置
+        this.r = 100; //物體的邊長
+        this.x = width;
+        this.y = height - this.r - 100;
+    }
+    move() {
+        // move會在sketch裡面不斷執行類似for迴圈
+        //這裡要一直更新他的y軸速度，包含重力以及速度
+        this.x -= 8; //給向上速度
+        //this.y = constrain(this.y, 0, height - this.r + 20);
+
+    }
+    show() {
+        image(cImg, this.x, this.y, this.r, this.r); //圖片/起始x/起始y/大小
+
+    }
+
+}
